6954d3ed7702f1ddfe3950e247d2a7e8a98c1cfb
Stephan Karl <steph@Stephans-MacBook-Pro.local>
2022-02-26 08:46:33 -0800

    large files commented out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 //Import files
-import ProfileGallery from './components/ProfileGallery'
+// import ProfileGallery from './components/ProfileGallery'
 import NavBar from './components/NavBar'
-import VideoContainer from './components/VideoContainer';
+// import VideoContainer from './components/VideoContainer';
 import Services from './components/Services'
-import Mission from './components/Mission'
-import About from './components/About';
+// import Mission from './components/Mission'
+// import About from './components/About';
 import InstaContainer from './components/InstaContainer';
 
 //Material UI
@@ -15,11 +15,11 @@ function App() {
   return (
     <Grid container>
       <NavBar />
-      <VideoContainer />
-      <ProfileGallery />
+      {/* <VideoContainer /> */}
+      {/* <ProfileGallery /> */}
       <Services />
-      <Mission />
-      <About />
+      {/* <Mission /> */}
+      {/* <About /> */}
       <InstaContainer />
     </Grid>
   );
